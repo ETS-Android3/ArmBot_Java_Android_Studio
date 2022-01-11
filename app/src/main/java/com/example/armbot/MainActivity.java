3449a35d402879b7b1216c7802da56020dc9e820
Anthony FERREYROLLES <anthony.ferreyrolles@edu.ece.fr>
2022-01-11 15:52:03 +0100

    Add Bluetooth activation request message

--- app/src/main/java/com/example/armbot/MainActivity.java
+++ app/src/main/java/com/example/armbot/MainActivity.java
@@ -17,13 +17,12 @@ import com.example.armbot.databinding.ActivityMainBinding;
 public class MainActivity extends AppCompatActivity {
 
     private AppBarConfiguration mAppBarConfiguration;
-    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        com.example.armbot.databinding.ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
         setSupportActionBar(binding.appBarMain.toolbar);
--- app/src/main/java/com/example/armbot/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/armbot/ui/home/HomeFragment.java
@@ -1,9 +1,22 @@
 package com.example.armbot.ui.home;
 
+import static android.content.ContentValues.TAG;
+
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothServerSocket;
+import android.bluetooth.BluetoothSocket;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -13,18 +26,20 @@ import androidx.navigation.fragment.NavHostFragment;
 import com.example.armbot.R;
 import com.example.armbot.databinding.FragmentHomeBinding;
 
+import java.io.IOException;
+import java.util.Set;
+import java.util.UUID;
+
 public class HomeFragment extends Fragment {
 
-    private HomeViewModel homeViewModel;
     private FragmentHomeBinding binding;
+    private final static int REQUEST_ENABLE_BT = 1;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        homeViewModel =
-                new ViewModelProvider(this).get(HomeViewModel.class);
+        HomeViewModel homeViewModel = new ViewModelProvider(this).get(HomeViewModel.class);
 
         binding = FragmentHomeBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
 
         /*final TextView textView = binding.textHome;
         homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
@@ -34,7 +49,7 @@ public class HomeFragment extends Fragment {
             }
         });*/
 
-        return root;
+        return binding.getRoot();
     }
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
@@ -47,6 +62,16 @@ public class HomeFragment extends Fragment {
                         .navigate(R.id.action_nav_home_to_nav_controller);
             }
         });
+
+        BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        // Device doesn't support Bluetooth
+        view.findViewById(R.id.enter_button).setEnabled(bluetoothAdapter != null);
+
+        assert bluetoothAdapter != null;
+        if (!bluetoothAdapter.isEnabled()) {
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+        }
     }
 
     @Override
